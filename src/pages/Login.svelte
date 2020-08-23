e881b011f062f4e5042c6804f0e4192aa3574ec7
Ranjit Daniel <ranjit@zag.team>
2020-08-23 13:35:35 +1200

    23082020

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -1,1 +1,82 @@
-<h1>welcome to login page</h1>
+<script>
+  import loginUser from "../strapi/loginUser";
+  import registerUser from "../strapi/registerUser";
+  var email = "";
+  var password = "";
+  var username = "default username";
+  var isMember = true;
+
+  $: isEmpty = !email || !password || !username;
+  //toggle member
+  function toggleMember() {
+    isMember = !isMember;
+    if (!isMember) {
+      username = "";
+    } else {
+      username = "default username";
+    }
+  }
+  //handle submit
+  async function handleSubmit() {
+    var user;
+    if (isMember) {
+      loginUser();
+    } else {
+      user = await registerUser({ email, password, username });
+    }
+    console.log(user);
+    if (user) {
+    } else {
+    }
+  }
+</script>
+
+<section class="form">
+  <h2 class="section-title">
+    {#if isMember}Sign in{:else}Register{/if}
+  </h2>
+  <form class="login-form" on:submit|preventDefault={handleSubmit}>
+    <!--single input-->
+    <div class="form-control">
+      <label for="email">email</label>
+      <input type="email" id="email" bind:value={email} />
+    </div>
+    <!--end of single input-->
+    <!--single input-->
+    <div class="form-control">
+      <label for="password">password</label>
+      <input type="password" id="password" bind:value={password} />
+    </div>
+    <!--end of single input-->
+    {#if !isMember}
+      <!--single input-->
+      <div class="form-control">
+        <label for="username">username</label>
+        <input type="text" id="username" bind:value={username} />
+      </div>
+      <!--end of single input-->
+    {/if}
+    {#if isEmpty}
+      <p class="form-empty">please fill out all the form fields</p>
+    {/if}
+    <button
+      type="submit"
+      class="btn btn-block btn-primary"
+      disabled={isEmpty}
+      class:disabled={isEmpty}>
+      submit
+    </button>
+    {#if isMember}
+      <p class="register-link">
+        need to register?
+        <button type="button" on:click={toggleMember}>click here</button>
+      </p>
+    {:else}
+      <p class="register-link">
+        already a member?
+        <button type="button" on:click={toggleMember}>click here</button>
+      </p>
+    {/if}
+
+  </form>
+</section>
---
+++ src/strapi/registerUser.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import url from './URL';
+
+async function registerUser({ email, password, username }) {
+    const response = await axios.post(`${url}/auth/local/register`, {
+        username,
+        email,
+        password
+    })
+        .catch(error => console.log(error));
+    if (response) {
+        //set up user
+    }
+
+    return response;
+}
+
+export default registerUser;
