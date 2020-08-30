886a45c35ad41d60a483fd8854bfa8c55e445776
Ranjit Daniel <ranjit@zag.team>
2020-08-31 00:22:19 +1200

    310820

--- src/pages/Checkout.svelte
+++ src/pages/Checkout.svelte
@@ -1,1 +1,21 @@
-<h1>welcome to checkout page</h1>
+<script>
+  import { onMount } from "svelte";
+  import { navigate, link } from "svelte-routing";
+  import user from "../stores/user";
+  import { cartTotal } from "../stores/cart";
+
+  onMount(() => {
+    if (!$user.jwt) {
+      navigate("/");
+    }
+  });
+</script>
+
+{#if $cartTotal > 0}
+  <h1>form</h1>
+{:else}
+  <div class="checkout-empty">
+    <h2>your cart is empty</h2>
+    <a href="/products" use:link class="btn btn-primary">fill it</a>
+  </div>
+{/if}
